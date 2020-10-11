850abd6d654a466a4057cebb6a46a973152a7039
Archit Pandey <architpandeynitk@gmail.com>
2020-10-11 21:34:10 +0530

    Updates

---
+++ precache-manifest.d5f766cd416f815a20b6e3a5eb60542e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c45b5cdaf5b393934fe36e40b6d66c9",
+    "url": "/css-framework-comparison/index.html"
+  },
+  {
+    "revision": "9accb8b3ab27e0ca567e",
+    "url": "/css-framework-comparison/static/css/main.c79df6ff.chunk.css"
+  },
+  {
+    "revision": "49c8f6e86cdcf8864c96",
+    "url": "/css-framework-comparison/static/js/2.bb29945f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/css-framework-comparison/static/js/2.bb29945f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9accb8b3ab27e0ca567e",
+    "url": "/css-framework-comparison/static/js/main.7e05283e.chunk.js"
+  },
+  {
+    "revision": "de7e7a753ef974b41423",
+    "url": "/css-framework-comparison/static/js/runtime-main.8a492fa8.js"
+  },
+  {
+    "revision": "20dd91153c78cd833e5d0cf461d87d2e",
+    "url": "/css-framework-comparison/static/media/github.20dd9115.svg"
+  },
+  {
+    "revision": "37ea68d7f38868c2ecc29720c8db74c1",
+    "url": "/css-framework-comparison/static/media/heart.37ea68d7.svg"
+  }
+]);
